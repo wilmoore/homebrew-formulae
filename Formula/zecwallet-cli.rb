3e564dd7ebdd7ff6b39b8fbe51c7db237310467c
Wil Moore III <wil.moore@wilmoore.com>
2021-06-22 19:29:53 -0400

    add prerelease to url for zecwallet-cli

--- Formula/zecwallet-cli.rb
+++ Formula/zecwallet-cli.rb
@@ -1,10 +1,10 @@
 class ZecwalletCli < Formula
   version "1.7.0"
-  prerelease "beta2"
+  @prerelease = "beta2"
 
   desc "ZecWallet Lightclient CLI"
   homepage "https://github.com/adityapk00/zecwallet-light-cli"
-  url "https://github.com/adityapk00/zecwallet-light-cli/releases/download/#{version}-#{prerelease}/macOS-zecwallet-cli-v#{version}.#{prerelease}.zip"
+  url "https://github.com/adityapk00/zecwallet-light-cli/releases/download/#{version}-#{prerelease}/macOS-zecwallet-cli-v#{version}.#{@prerelease}.zip"
   sha256 "23e3de1105dd4d735ee2e042c1184e8f57ce41c68afa3fd1a104d5ad0c35e355"
   license "MIT"
 
